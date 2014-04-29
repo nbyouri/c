34a3316259cf4bbfd30606e29c104775f65c1f42
yrmt <youri.mout@gmail.com>
2014-04-29 22:23:08 +0200

    fclose oublie

--- fichiers/fopen.c
+++ fichiers/fopen.c
@@ -10,39 +10,40 @@ void create_file(const char *);
 void addtofile(const char *, const char *);
 
 void read_file(const char *filename) {
-	FILE *file;
-	char s[MAX];
-	if ((file = fopen(filename, "rt")) == NULL)
-		exit(EXIT_FAILURE);
-	while (fgets(s, sizeof(s), file)) {
-		printf("%s", s);
-	}
+    FILE *file;
+    char s[MAX];
+    if ((file = fopen(filename, "rt")) == NULL)
+        exit(EXIT_FAILURE);
+    while (fgets(s, sizeof(s), file)) {
+        printf("%s", s);
+    }
+    fclose(file);
 }
 
 void create_file(const char *filename) {
-	FILE *file;
-	if ((file = fopen(filename, "wt")) == NULL)
-		exit(EXIT_FAILURE);
-	fclose(file);
+    FILE *file;
+    if ((file = fopen(filename, "wt")) == NULL)
+        exit(EXIT_FAILURE);
+    fclose(file);
 }
 
 void addtofile(const char *filename, const char *line) {
-	FILE *file;
-	if ((file = fopen(filename, "at")) == NULL)
-		exit(EXIT_FAILURE);
-	fprintf(file, "%s\n", line);
-	fclose(file);
+    FILE *file;
+    if ((file = fopen(filename, "at")) == NULL)
+        exit(EXIT_FAILURE);
+    fprintf(file, "%s\n", line);
+    fclose(file);
 }
 
 int main(void) {
-	char filename[] = "lel.txt";
-	char line[MAX] = "youri";
-	char adjectif[4][MAX] = { "cool", "keks", "sexy", "ok" };
-	create_file(filename);
-	for (int i = 0; i < taille(adjectif); i++) {
-		strcat(line, adjectif[i]);
-		addtofile(filename, line);
-	}
-	read_file(filename);
-	return 0;
+    char filename[] = "lel.txt";
+    char line[MAX] = "youri";
+    char adjectif[4][MAX] = { "cool", "keks", "sexy", "ok" };
+    create_file(filename);
+    for (int i = 0; i < taille(adjectif); i++) {
+        strcat(line, adjectif[i]);
+        addtofile(filename, line);
+    }
+    read_file(filename);
+    return 0;
 }
