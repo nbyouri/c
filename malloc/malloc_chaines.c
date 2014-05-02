f4f19d51ef4d0637bfeeca5e7de72d3cce20886c
yrmt <youri.mout@gmail.com>
2014-05-02 13:18:28 +0200

    Ajout de reallocation du tableau de chaine de caracteres

--- malloc/malloc_chaines.c
+++ malloc/malloc_chaines.c
@@ -6,25 +6,44 @@
 #define MAX 10
 
 void afficher_tab(char **, int);
+void initialiser(char **, int, int);
 
 void afficher_tab(char **tab, int nb) {
 	for (int i = 0; i < nb; printf("%s\n", *(tab + i++)));
 }
 
-int main(void) {
-	char **tab;
-	int nb;
-	int i;
-	printf("Combien de chaines de car? : ");
-	scanf("%d", &nb);
-	if ((tab = (char **)malloc(MAX * sizeof(char **))) == NULL)
-		return -1;
-	for (i = 0; i < nb; i++) {
+void initialiser(char **tab, int nb, int n) {
+	int i = 0;
+	for (i = nb; i < nb + n; i++) {
 		printf("chaine nr.%d? : ", i);
 		tab[i] = malloc(MAX_CAR);
 		scanf("%s", tab[i]);
 	}
+}
+
+int main(void) {
+	char **tab;
+	char **retab;
+	int nb;
+	int n;
+	int i;
+
+	// allocation initiale
+	printf("Combien de chaines de car? : ");
+	scanf("%d", &nb);
+	if ((tab = (char **)malloc(MAX * sizeof(char **))) == NULL)
+		return -1;
+	initialiser(tab, 0, nb);
 	afficher_tab(tab, nb);
+
+	// reallocation; on rajoute des elements
+	printf("Combien de chaines voulez vous en plus? : ");
+	scanf("%d", &n);
+	if ((retab = (char **)realloc(tab, n)) == NULL)
+		return -1;
+	tab = retab;
+	initialiser(tab, nb, n);
+	afficher_tab(tab, n+nb);
 	free(tab);
 	return 0;
 }
