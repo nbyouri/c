ab8cfb25017455e0299f293653114f4dd730e6b7
yrmt <youri.mout@gmail.com>
2014-06-09 18:31:25 +0200

    dont quit if search fails

--- revision/search.c
+++ revision/search.c
@@ -7,7 +7,7 @@ void search_num(student * class, id_num * id, unsigned int nbid) {
     temp = bsearch(&key, id, nbid, sizeof(id_num), sort_index_num);
     if (temp == NULL) {
         // exit if bsearch fails
-        error("failed to bsearch\n");
+        puts("not found\n");
     } else {
         // otherwise, print the students matching the index key
         unsigned int i = 0;
